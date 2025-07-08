a66ec56ce6eae8a652caedbb14adb95686cbedb5
João Ricardo <joaoricardo.ra1106@gmail.com>
2025-07-08 10:42:30 -0300

    criado pasta para guardar funções

--- imc.py
+++ imc.py
@@ -1,17 +1,4 @@
-def ler_float(mensagem: str, valorMinimo: float = None, valorMaximo: float = None) -> float:
-    while True:
-        try:
-            valor = float(input(mensagem))
-            if valorMinimo is not None and valorMaximo is not None:
-                if valorMinimo <= valor <= valorMaximo:
-                    return valor
-                else:
-                    print(f"❌ O valor deve estar entre {valorMinimo} e {valorMaximo}.")
-            else:
-                return valor
-        except ValueError:
-            print("❌ Entrada inválida. Digite um número válido (ex: 70.5).")
-
+from myFunctions.ler_float import ler_float
 
 def ler_nome(mensagem: str) -> str:
     while True:
